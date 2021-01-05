d799537eda1ef1deae302a2e20453d44b71f455f
t4h4 <thyasinerkan@gmail.com>
2021-01-05 22:56:38 +0300

    head added and insert in layout master page

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,12 @@
 import Navigation from "./navigation";
+import Head from 'next/head';
 function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <title>Taha's Web Site</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navigation />
       <main>{children}</main>
       <footer>2021</footer>
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
 import Layout from "../components/layout";
+import Head from 'next/head';
 function HomePage() {
   return (
     <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
       <h1>This is About</h1>
     </Layout>
   );
